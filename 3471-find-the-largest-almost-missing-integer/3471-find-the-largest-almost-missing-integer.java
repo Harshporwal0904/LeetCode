f1242b467e03105b7481145ca7b84fd0867e2dca
Harsh Porwal <144673012+Harshporwal0904@users.noreply.github.com>
2026-08-18 23:17:49 +0530

    Time: 1 ms (99.3%), Space: 44.3 MB (97.54%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        
+        int n = nums.length;
+
+        // Case 1: only one subarray exists
+        if (k == n) {
+            int ans = 0;
+
+            for (int num : nums) {
+                ans = Math.max(ans, num);
+            }
+
+            return ans;
+        }
+
+        int[] freq = new int[51];
+
+        // Count frequency in the entire array
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Case 2: every element itself is a subarray
+        if (k == 1) {
+            int ans = -1;
+
+            for (int num : nums) {
+                if (freq[num] == 1) {
+                    ans = Math.max(ans, num);
+                }
+            }
+
+            return ans;
+        }
+
+        // Case 3: 1 < k < n
+        int ans = -1;
+
+        if (freq[nums[0]] == 1) {
+            ans = Math.max(ans, nums[0]);
+        }
+
+        if (freq[nums[n - 1]] == 1) {
+            ans = Math.max(ans, nums[n - 1]);
+        }
+
+        return ans;
+    }
+}
